e48cb3d3411127912f62e02f2d3cc1110340e260
augustyn1331 <augustyn1331@gmail.com>
2021-07-05 12:50:20 +0200

    react-native-sensors installed

--- src/screens/Landing/index.tsx
+++ src/screens/Landing/index.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import {Image, Text, View} from 'react-native';
 import Container from '../../components/Container';
 import styles from './styles';
+import { accelerometer } from "react-native-sensors";
+
+// const subscription = accelerometer.subscribe(({ x, y, z, timestamp }) =>
+//   console.log({ x, y, z, timestamp })
+// );
 
 interface IProps {}
 
